bb6c4527c026e5e2e6dd3c59261db289a11515f8
Yaxi_Zhang <abbyzhang2015@hotmail.com>
2018-04-20 16:39:20 +0200

    Changed the Database to MySQL and add 404 handler

--- models.py
+++ models.py
@@ -4,8 +4,7 @@ from flask_bcrypt import generate_password_hash
 from flask_login import UserMixin
 from peewee import *
 
-
-DATABASE = SqliteDatabase('social.db')
+DATABASE = MySQLDatabase('social', user='root', passwd='pass12345')
 
 
 class User(UserMixin, Model):
@@ -24,7 +23,24 @@ class User(UserMixin, Model):
 
     def get_stream(self):
         return Post.select().where(
+            (Post.user << self.following()) |
             (Post.user == self)
+        )
+
+    def following(self):
+        """The users that we are following."""
+        return (
+            User.select().join(Relationship, on=Relationship.to_user).where(
+                Relationship.from_user == self
+            )
+        )
+
+    def followers(self):
+        """Get users following the current user"""
+        return (
+            User.select().join(Relationship, on=Relationship.from_user).where(
+                Relationship.to_user == self
+            )
         )
 
     @classmethod
@@ -49,7 +65,18 @@ class Post(Model):
         order_by = ('-timestamp',)
 
 
+class Relationship(Model):
+    from_user = ForeignKeyField(User, related_name='relationships')
+    to_user = ForeignKeyField(User, related_name='related_to')
+
+    class Meta:
+        database = DATABASE
+        indexes = (
+            (('from_user', 'to_user'), True),
+        )
+
+
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([User, Post], safe=True)
+    DATABASE.create_tables([User, Post, Relationship], safe=True)
     DATABASE.close()
